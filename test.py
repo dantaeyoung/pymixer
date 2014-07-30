acd087e9d9312ac435780f6a510c41d894a427ac
Dan Taeyoung <provolot@gmail.com>
2014-07-30 19:04:23 -0400

    initial test working

--- test.py
+++ test.py
@@ -1,10 +1,48 @@
+import copy
+import random
+from glob import glob
 from pydub import AudioSegment
 
-sound1 = AudioSegment.from_mp3("./mp3s/1.mp3")
-sound2 = AudioSegment.from_mp3("./mp3s/2.mp3")
+mp3dir = "./mp3s/"
+# pydub does things in milliseconds
+slice_duration = 0.5 * 1000;
+segment_count = 50
 
-# mix sound2 with sound1, starting at 5000ms into sound1)
-output = sound1.overlay(sound2, position=5000)
 
+print "loading all songs"
+allsongs = []
+for mp3_file in glob(mp3dir + "*.mp3"):
+	print "loading", mp3_file
+	allsongs.append(AudioSegment.from_mp3(mp3_file))
+
+#randsong = None
+randsong = AudioSegment.empty()
+
+for i in xrange(segment_count):
+
+	print "#pick a song"
+	#pick a song
+	thissong = random.choice(allsongs)
+	print "we picked", thissong
+
+	print "#get its length (in milliseconds)"
+	#get its length (in milliseconds)
+	thislen = len(thissong)
+	print "len is", thislen
+
+	print "#get slice randomly"
+	#get slice randomly
+	slice_start = random.randrange(0, thislen - slice_duration)
+	print "slice is from", slice_start / 1000.0, "to", (slice_start + slice_duration) / 1000.0, "secs"
+
+	print "#add random slice to total"
+	#add random slice to total
+	print "adding another song"
+	randsong += thissong[slice_start:(slice_start + slice_duration)]
+
+	print "this song is now", len(randsong) / 1000, "seconds"
+
+
+print "saving"
 # save the result
-output.export("./mixed_sounds.mp3", format="mp3")
+randsong.export("./mixed_sounds.mp3", format="mp3")
